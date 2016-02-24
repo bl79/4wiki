db233bd937d07dbc34cd290634ae9c01d35d5700
bl79 <blagopoluchie12@gmail.com>
2016-02-24 12:26:42 +0300

    u

---
+++ AWB_sectionsSO2DO.php
@@ -0,0 +1,34 @@
+﻿<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /><title></title></head><body>
+<?php
+
+require 'tsd-vars.php';
+
+
+$txt = "<section begin=\"Армада\" />'''Арма{{акут}}да, ''армадилъ''''' <small>ипр.</small>, см. ''{{tsdl|армія}}''.{{tsdbr}}{{tsdbr}}<section end=\"Армада\" />";
+
+
+
+
+// <section begin="([^"]*[^\d])" */ *>
+$txt = preg_replace_callback('~(<section begin="([^"]*(?:[а-яёѣѵіѳА-ЯЁѢѴІѲ-](?: \d)?|[^а-яёѣѵіѳА-ЯЁѢѴІѲ-]\d))" */ *>\s*)(.*?)(\s*<section end="\2" */ *>)~usi',
+		function ($s) {
+			$t = $s[3];
+			//$t = addslashes($t);
+			$t = oformlenie($t);
+			$t = toDO($t);
+			//$t = stripslashes($t);
+			//echo htmlspecialchars($t);
+			return $s[1].$t.$s[4];},
+		$txt);
+
+
+printf ($txt);
+
+
+function replace($t)
+{
+	//$t = $t[0].'iiii';
+	$t = 'iiii';
+  return $t;
+
+}
