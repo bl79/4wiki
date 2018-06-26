1815cb99e4090bc04fbfadfbfe481d133dc6d510
Vlad <blagopoluchie12@gmail.com>
2018-06-26 17:37:03 +0300

    codestyling

--- categories_rename/pwb_category.py
+++ categories_rename/pwb_category.py
@@ -19,61 +19,56 @@ from vladi_commons.vladi_helpers import file_readlines_in_list_interlines
 # summary = r"переименование категории: аналогично другим в [[:Категория:Праздники по странам]]"
 summary = r"категория"
 
+
 # регулярка для создания списка (в отдельном файле, незабыть переконвертировать список в оконания строк как в Unix CR для r'\n'):
 # ^Категория:Арбитры(.*?)\n
 # Категория:Футбольные арбитры$1\nКатегория:Футбольные судьи$1\n
 
-file_listcat = 'cats2rename.txt'
-CategoriesToRename = file_readlines_in_list_interlines(file_listcat)
-
-Windows = False
-# Windows = True
-if Windows:
-    basepath = 'c:'
-    runcommand = f'python {basepath}/pwb/pwb.py '
-    config = ''
-else:
-    # basepath = '/mnt/win-c'
-    # basepath = '~'
-    runcommand = 'python3 $PWBPATH/pwb.py '
-    config = ' -dir:~/.pywikibot'
-args = [
-    '-family:wikisource',
-    # '-simulate',
-]
-arguments = ' ' + ' '.join(args)
-
-
 def clearstr(s):
+    """чистка невидимого символа в конце названия"""
     return s.strip().replace(b'\xe2\x80\x8e'.decode('utf-8'), '')
 
 
-# логин
-run = f'{runcommand} login.py {arguments} {config}'
-os.system(run)
+if __name__ == '__main__':
 
-# print('echo ' + str(CategoriesToRename))	
-summary_ = f' -summary:"{summary}"'
-for cats in CategoriesToRename:
-    # переименование страницы
-    command = r'movepages.py -pt:0 -noredirect'
-    from_ = f' -from:"{clearstr(cats[0])}"'
-    to_ = f' -to:"{clearstr(cats[1])}"'
-    run = runcommand + command + from_ + to_ + summary_ + arguments + config
-    print('echo ' + run)
+    file_listcat = 'cats2rename.txt'
+    CategoriesToRename = file_readlines_in_list_interlines(file_listcat)
+    CategoriesToRename = [(clearstr(pair[0]), clearstr(pair[0])) for pair in CategoriesToRename]
+
+    Windows = False
+    # Windows = True
+    if Windows:
+        runcommand, config = f'python c:/pwb/pwb.py', ''
+    else:
+        runcommand, config = 'python3 $PWBPATH/pwb.py', '-dir:~/.pywikibot'
+    args = [
+        '-family:wikisource',
+        '-simulate',
+        '-user:VladiBot',
+    ]
+    arguments = ' ' + ' '.join(args)
+
+    # логин
+    run = f'{runcommand} login.py {arguments} {config}'
     os.system(run)
 
-    # переименование категорий с невидимым символом в конце названия
-    command = r'replace.py -regex "(\[\[Категория:[^]|]+)[‎\s]+(\||\]\])" "\1\2"'
-    cat_ = ' -cat:"' + clearstr(cats[0]) + '"'
-    run = runcommand + command + cat_ + ' -always -summary:"викификация"' + arguments + config
-    print('echo ' + run)
-    os.system(run)
+    # print('echo ' + str(CategoriesToRename))
+    summary_ = f' -summary:"{summary}"'
+    for catold, catnew in CategoriesToRename:
+        # переименование страницы
+        command = r'movepages.py -pt:0 -noredirect'
+        run = f'{runcommand} {command} -from:"{catold}" -to:"{catnew}" {summary_} {arguments} {config}'
+        print(f'echo {run}')
+        os.system(run)
 
-    # переименование категорий
-    command = r'category.py move -pt:0 -inplace'  # -keepsortkey
-    from_ = ' -from:"' + clearstr(cats[0]) + '"'
-    to_ = ' -to:"' + clearstr(cats[1]) + '"'
-    run = runcommand + command + from_ + to_ + summary_ + arguments + config
-    print('echo ' + run)
-    os.system(run)
+        # переименование категорий
+        command = r'replace.py -regex "(\[\[Категория:[^]|]+)[‎\s]+(\||\]\])" "\1\2"'
+        run = f'{runcommand} {command} -cat:"{catold}" -always -summary:"викификация" {arguments} {config}'
+        print(f'echo {run}')
+        os.system(run)
+
+        # переименование категорий
+        command = r'category.py move -pt:0 -inplace'  # -keepsortkey
+        run = f'{runcommand} {command} -from:"{catold}" -to:"{catnew}" {summary_} {arguments} {config}'
+        print(f'echo {run}')
+        os.system(run)
